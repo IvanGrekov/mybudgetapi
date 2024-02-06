64162cb1ca60d2c81716f292f2b26d9cc7a41c2f
Ivan Grekov <ivan.grekov@windmill.digital>
2024-02-06 22:31:15 +0200

    using DB for users service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,29 +19,32 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll(@Query() query): User[] {
+  findAll(@Query() query): Promise<User[]> {
     const { limit, page } = query || {};
 
     return this.usersService.findAll(limit, page);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number): User {
+  findOne(@Param('id') id: number): Promise<User> {
     return this.usersService.findOne(id);
   }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto): User {
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Patch(':id')
-  editOne(@Param('id') id: number, @Body() editUserDto: EditUserDto): User {
+  editOne(
+    @Param('id') id: number,
+    @Body() editUserDto: EditUserDto,
+  ): Promise<User> {
     return this.usersService.edit(id, editUserDto);
   }
 
   @Delete(':id')
-  deleteOne(@Param('id') id: number): User {
+  deleteOne(@Param('id') id: number): Promise<User> {
     return this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,39 +1,31 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
-import { User, ELanguage } from './entities/user.entity';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user-dto/create-user.dto';
 import { EditUserDto } from './dto/create-user-dto/edit-user.dto';
-import { ECurrency } from '../shared/enums/currency.enum';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [
-    {
-      id: 1,
-      nickname: 'john',
-      defaultCurrency: ECurrency.USD,
-      language: ELanguage.EN,
-    },
-    {
-      id: 2,
-      nickname: 'chris',
-      defaultCurrency: ECurrency.EUR,
-      language: ELanguage.EN,
-    },
-    {
-      id: 3,
-      nickname: 'maria',
-      defaultCurrency: ECurrency.UAH,
-      language: ELanguage.UA,
-    },
-  ];
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-  findAll(limit = 5, page = 1): User[] {
-    return this.users.slice((page - 1) * limit, page * limit);
+  async findAll(limit = 5, page = 1): Promise<User[]> {
+    const users = await this.userRepository.find({
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    return users;
   }
 
-  findOne(id: User['id']): User {
-    const user = this.users.find((user) => user.id === id);
+  async findOne(id: User['id']): Promise<User> {
+    const user = await this.userRepository.findOneBy({
+      id,
+    });
 
     if (!user) {
       throw new NotFoundException(`User #${id} not found`);
@@ -42,41 +34,28 @@ export class UsersService {
     return user;
   }
 
-  create(createUserDto: CreateUserDto): User {
-    const newUser = {
-      id: this.users.length + 1,
-      ...createUserDto,
-    };
-    this.users.push(newUser);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(createUserDto);
 
-    return newUser;
+    return this.userRepository.save(user);
   }
 
-  edit(id: User['id'], editUserDto: EditUserDto): User {
-    const index = this.users.findIndex((user) => user.id === id);
-
-    if (index === -1) {
-      throw new NotFoundException(`User #${id} not found`);
-    }
-
-    this.users[index] = {
-      ...this.users[index],
+  async edit(id: User['id'], editUserDto: EditUserDto): Promise<User> {
+    const user = await this.userRepository.preload({
+      id,
       ...editUserDto,
-    };
+    });
 
-    return this.users[index];
-  }
-
-  delete(id: User['id']): User {
-    const index = this.users.findIndex((user) => user.id === id);
-
-    if (index === -1) {
+    if (!user) {
       throw new NotFoundException(`User #${id} not found`);
     }
 
-    const deletedUser = this.users[index];
-    this.users.splice(index, 1);
+    return this.userRepository.save(user);
+  }
 
-    return deletedUser;
+  async delete(id: User['id']): Promise<User> {
+    const user = await this.findOne(id);
+
+    return this.userRepository.remove(user);
   }
 }
