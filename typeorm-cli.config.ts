a965dc54ad374986b5160daf67eff14a78429071
Ivan Grekov <ivan.grekov@windmill.digital>
2024-04-07 10:50:23 +0300

    prettier and eslint was added

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,12 +1,12 @@
 import { DataSource } from 'typeorm';
 
 export default new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  database: 'postgres',
-  username: 'postgres',
-  password: 'Cosonic56',
-  entities: [],
-  migrations: [],
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    username: 'postgres',
+    password: 'Cosonic56',
+    entities: [],
+    migrations: [],
 });
