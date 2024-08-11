c427b5c4f85ae71d7f6ee7556c9349154eb5046d
Ivan Grekov <ivan.grekov@windmill.digital>
2024-08-11 15:03:20 +0300

    additional envs added

--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -2,9 +2,9 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.DB_HOST,
     port: parseInt(process.env.DB_CONTAINER_PORT),
-    database: 'postgres',
+    database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     entities: [],
