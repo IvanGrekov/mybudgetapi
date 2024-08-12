21abc668f09db7b79e7656b4ec32a5d5a311e952
Ivan Grekov <ivan.grekov@windmill.digital>
2024-08-12 17:59:25 +0300

    env validation with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,22 @@ import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { TransactionCategoriesModule } from './transaction-categories/transaction-categories.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('development', 'production').required(),
+                DB_HOST: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+                DB_IMAGE_NAME: Joi.string().required(),
+                DB_CONTAINER_PORT: Joi.number().required(),
+                DB_CONTAINER_INTERNAL_PORT: Joi.number().required(),
+                DB_USER: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+            }),
+        }),
         TypeOrmModule.forRoot({
             type: 'postgres',
             host: process.env.DB_HOST,
