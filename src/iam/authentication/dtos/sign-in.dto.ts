8ec3414836a25316f4ed7c546632102a752e3c86
Ivan Grekov <ivan.grekov@windmill.digital>
2024-10-08 21:51:07 +0300

    fix SignInDto validator decorators

--- src/iam/authentication/dtos/sign-in.dto.ts
+++ src/iam/authentication/dtos/sign-in.dto.ts
@@ -1,9 +1,12 @@
-import { IsOptional, IsNumberString } from 'class-validator';
+import { IsString, IsOptional, IsNumberString } from 'class-validator';
 import { PickType } from '@nestjs/swagger';
 
 import { CreateUserDto } from '../../../shared/dtos/create-user.dto';
 
-export class SignInDto extends PickType(CreateUserDto, ['email', 'password']) {
+export class SignInDto extends PickType(CreateUserDto, ['email']) {
+    @IsString()
+    readonly password: string;
+
     @IsOptional()
     @IsNumberString()
     readonly tfaToken?: string;
