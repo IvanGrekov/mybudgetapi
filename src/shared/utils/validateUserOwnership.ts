d0cffe8c970cc5af4810769fe4d9f6b1ef2b971c
Ivan Grekov <ivan.grekov@windmill.digital>
2025-01-29 20:37:11 +0200

    fix validateUserOwnership util

--- src/shared/utils/validateUserOwnership.ts
+++ src/shared/utils/validateUserOwnership.ts
@@ -3,10 +3,10 @@ import { ForbiddenException } from '@nestjs/common';
 import { User } from 'shared/entities/user.entity';
 import { EUserRole } from 'shared/enums/user-role.enums';
 
-type TValidateUserOwnership = (args: { activeUserId?: number; entity: { user: User } }) => void;
+type TValidateUserOwnership = (args: { activeUserId?: number; entity?: { user: User } }) => void;
 
 export const validateUserOwnership: TValidateUserOwnership = ({ activeUserId, entity }) => {
-    if (typeof activeUserId !== 'number') {
+    if (typeof activeUserId !== 'number' || !entity) {
         return;
     }
 
